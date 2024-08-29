16c2fed0dc06e7c07ca11382a3de0cf9995c0091
AshharAli-18 <iashharali.18@gmail.com>
2024-08-29 10:08:42 +0500

    Updated feature/authenticaton

--- src/main/java/com/yolo/chef/ChefApplication.java
+++ src/main/java/com/yolo/chef/ChefApplication.java
@@ -12,6 +12,10 @@ public class ChefApplication {
 		String dbUrl = dotenv.get("DB_URL");
 		String dbUsername = dotenv.get("DB_USERNAME");
 		String dbPassword = dotenv.get("DB_PASSWORD");
+		String apiKey = dotenv.get("ApiKey");
+		String geminiUrl = dotenv.get("GeminiUrl");
+		String jwtIssuerUri = dotenv.get("JwtIssuerURI");
+		String jwtSetUri = dotenv.get("JwtSetURI");
 
 		if (dbUrl == null || dbUsername == null || dbPassword == null) {
 			throw new IllegalStateException("Missing required environment variables: DB_URL, DB_USERNAME, or DB_PASSWORD.");
@@ -20,6 +24,10 @@ public class ChefApplication {
 		System.setProperty("DB_URL", dbUrl);
 		System.setProperty("DB_USERNAME", dbUsername);
 		System.setProperty("DB_PASSWORD", dbPassword);
+		System.setProperty("ApiKey", apiKey);
+		System.setProperty("GeminiUrl", geminiUrl);
+		System.setProperty("JwtIssuerURI", jwtIssuerUri);
+		System.setProperty("JwtSetURI", jwtSetUri);
 
 		SpringApplication.run(ChefApplication.class, args);
 	}
